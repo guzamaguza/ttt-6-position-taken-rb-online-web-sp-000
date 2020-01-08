2666a3109611576e215ba1f4a897e121f5164d9c
Mark A Guza <guzamaguza@gmail.com>
2020-01-08 05:39:35 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,24 @@
 # code your #position_taken? method here!
 
-def position_taken?()
+def position_taken?(board, index)
 
+  if (board == [] && index == 0){
+    return 
+  }
 
+  if (board == [" ", " ", " ", " ", " ", " ", " ", " ", " "] && index == 0){
+    return false
+  }
+
+  for(let i=0;i<board.length;i++){
+    if (board[i] = nil || board[i] == ){
+      return false
+    }elsif(board[i] == "X" || board[i] == "O"){
+      return true 
+    }
+    
+  }
+
+  
 
 end
